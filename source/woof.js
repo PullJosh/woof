c35eb0de9ef4647ae98caa30dea3dcb84a4ad1f3
Josh Pullen <JoshPullen27@gmail.com>
2016-05-28 10:56:37 -0400

    Delete woof.js

--- source/woof.js
+++
@@ -1,5 +0,0 @@
-/* Woof! */
-document.head.innerHTML += '<!-- Woof! -->\n<style id="woof">' + css + '</style>'
-
-if(window.jQuery) document.body.classList.add('woof')
-else document.body.classList.add('woof-new')
